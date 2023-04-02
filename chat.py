54966cf51dda0afba128d5a9bc46f1558fb9c396
Pierce Cohen <pierce@piercecohen.com>
2023-04-01 23:14:53 -0500

    Use prompt toolkit instead of readline for better tab completion and cross-platform support

--- chat.py
+++ chat.py
@@ -13,12 +13,20 @@ from playsound import playsound
 import argparse
 import json
 from rich.table import Table
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import PathCompleter, Completer, Completion
+from prompt_toolkit.document import Document
 
-if os.name == "nt":
-    import pyreadline as readline
-else:
-    import gnureadline as readline
+class CustomPathCompleter(Completer):
+    def get_completions(self, document, complete_event):
+        text = document.text_before_cursor
 
+        if text.startswith("/save ") or text.startswith("/load "):
+            command, partial_path = text.split(" ", 1)
+            path_completer = PathCompleter()
+            path_document = Document(partial_path, len(partial_path))
+            for completion in path_completer.get_completions(path_document, complete_event):
+                yield Completion(completion.text, completion.start_position, completion.display, completion.display_meta)
 
 # OpenAI API key, stored as an environment variable
 openai.api_key = os.environ.get("OPENAI_API_KEY")
@@ -45,7 +53,7 @@ def file_completion(text, state):
     return files[state] if state < len(files) else None
 
 class ChatApplication:
-    def __init__(self, system_message=None, model="gpt-3.5-turbo"):
+    def __init__(self, system_message=None, model="gpt-4"):
         self.model = model
         self.system_message = system_message
         self.initialize_messages()
@@ -110,6 +118,15 @@ class ChatApplication:
 
     def save_chat(self, filename):
         try:
+            abs_path = os.path.abspath(filename)
+            if os.path.isdir(abs_path):
+                print("Error: The specified path is a directory, please provide a valid file name.")
+                return
+
+            base_path = os.path.dirname(abs_path)
+            if not os.path.exists(base_path):
+                os.makedirs(base_path)
+
             # Remove the model information from the messages
             for message in self.messages:
                 if "model" in message:
@@ -122,7 +139,7 @@ class ChatApplication:
 
             with open(filename, "w") as outfile:
                 json.dump(chat_data, outfile)
-            
+
             print(f"Chat saved to '{filename}'.")
         except Exception as e:
             print(f"An error occurred while saving the chat: {e}")
@@ -130,6 +147,7 @@ class ChatApplication:
     def load_chat(self, filename):
         clear_terminal()
         try:
+            filename = os.path.abspath(filename)
             with open(filename, "r") as infile:
                 chat_data = json.load(infile)
 
@@ -175,7 +193,7 @@ def main():
     args = parser.parse_args()
 
     if args.version:
-        print("GPT-CLI version 1.2.0")
+        print("GPT-CLI version 1.2.5")
         sys.exit(0)
         
     chat_app = ChatApplication()
@@ -183,18 +201,20 @@ def main():
     last_response = ""
     print("Model: " + chat_app.model)
 
-    readline.set_completer(file_completion)
-    readline.parse_and_bind("tab: complete")
+    session = PromptSession(completer=CustomPathCompleter())
 
     try:
         while True:
-            user_message = input("You: ")
+            user_message = session.prompt("You: ")
 
             if "/paste" in user_message:
                 user_message = user_message.replace("/paste", pyperclip.paste())
 
             if user_message.startswith("/"):
-                commands = user_message.split("/")
+                if user_message.startswith("/save ") or user_message.startswith("/load "):
+                    commands = [user_message[1:]]
+                else:
+                    commands = user_message.split("/")
 
                 new_system_message = None
                 new_model = None
@@ -272,15 +292,22 @@ def main():
                         table.add_row("Assistant Messages", str(assistant_messages))
 
                         chat_app.console.print(table)
-                    elif command.startswith("save"):
-                        filename = command[4:].strip()
+                    elif user_message.startswith("/save "):
+                        filename = user_message[5:].strip()
                         if filename:
-                            chat_app.save_chat(filename)
+                            if not os.path.dirname(filename):
+                                filename = os.path.join(os.getcwd(), filename)
+                            if os.path.isdir(filename):
+                                print("Error: The specified path is a directory, please provide a valid file name.")
+                            else:
+                                chat_app.save_chat(filename)
                         else:
                             print("Please specify a filename after the /save command.")
-                    elif command.startswith("load"):
-                        filename = command[4:].strip()
+                    elif user_message.startswith("/load "):
+                        filename = user_message[5:].strip()
                         if filename:
+                            if not os.path.dirname(filename):
+                                filename = os.path.join(os.getcwd(), filename)
                             chat_app.load_chat(filename)
                         else:
                             print("Please specify a filename after the /load command.")
